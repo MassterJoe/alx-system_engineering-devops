19adf5e1fc161f3185d36824107c17fbf4c2d4f7
Salawu O Joseph <sojbaba98@gmail.com>
2023-04-24 11:33:27 -0400

    finally

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,11 +4,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    # get args
     employee_id = sys.argv[1]
+    # fetch url from a fake api
     url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
     response = requests.get(url)
     todos = response.json()
 
+    # get the count for number of completed to-dos
     num_completed = 0
     for todo in todos:
         if todo["completed"]:
@@ -17,6 +20,7 @@ if __name__ == "__main__":
     num_tasks = len(todos)
     url = f"https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
     response = requests.get(url)
+    # get employee name and print the result
     employee_name = response.json()["name"]
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, num_completed, num_tasks))
