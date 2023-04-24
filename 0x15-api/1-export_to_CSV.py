15364ce5a7d725702868a842bf417375d96bbdcc
Salawu O Joseph <sojbaba98@gmail.com>
2023-04-24 11:14:43 -0400

    great

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""
-Write a Python script that, using this REST API,
+"""Write a Python script that, using this REST API,
 for a given employee ID, returns information about his/her
 TODO list progress.
 """
+import csv
 import requests
 import sys
-import csv
+
+
 if __name__ == "__main__":
     employee_id = sys.argv[1]
     url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
@@ -19,16 +20,14 @@ if __name__ == "__main__":
             num_completed += 1
 
     num_tasks = len(todos)
-    # Get the name of the employee
-    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
     response = requests.get(url)
     employee_name = response.json()["name"]
-    csv_data = [["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]]
-
+    csv_data = []
     for todo in todos:
         csv_data.append(
             [employee_id, employee_name, todo['completed'], todo['title']])
-    file_name = f"{employee_id}.csv"
+    file_name = "{}.csv".format(employee_id)
 
     with open(file_name, "w", newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
